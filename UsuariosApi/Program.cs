cc31317f96c77c5b7baf1826ceb27cca508e667d
Thiago Santos <thiago.santos.gomes.00@gmail.com>
2025-03-12 11:46:03 -0300

    02.01 - Criando Usuario

--- UsuariosApi/Program.cs
+++ UsuariosApi/Program.cs
@@ -21,6 +21,10 @@ builder.Services
                 .AddEntityFrameworkStores<UsuarioDbContext>()
                     .AddDefaultTokenProviders();
 
+builder.Services.AddAutoMapper
+    (AppDomain.CurrentDomain.GetAssemblies());
+
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
