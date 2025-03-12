b57df2a9c2a482949de2e18d394340c41ef872e3
Thiago Santos <thiago.santos.gomes.00@gmail.com>
2025-03-12 12:11:30 -0300

    02.02 - Movendo para um Serviço

--- UsuariosApi/Program.cs
+++ UsuariosApi/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using UsuariosApi.Data;
 using UsuariosApi.Models;
+using UsuariosApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -24,6 +25,7 @@ builder.Services
 builder.Services.AddAutoMapper
     (AppDomain.CurrentDomain.GetAssemblies());
 
+builder.Services.AddScoped<CadastroService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
